c93ffbde32d46c1790a1e871307ff4260650602a
yjyszzr <1053195207@qq.com>
2020-02-13 23:39:56 +0800

    Update EurekaServer.java

--- src/main/java/com/dl/EurekaServer.java
+++ src/main/java/com/dl/EurekaServer.java
@@ -8,6 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServer {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(EurekaServer.class).web(true).run(args);
+		 SpringApplication.run(EurekaServer.class).run(args);
 	}
 }
